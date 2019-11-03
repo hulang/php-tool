bc2f25eeec59d51921b2ae67583d82a47d449512
hulangfy <hulangfy@163.com>
2019-11-03 09:53:54 +0800

    update

---
+++ src/File.php
@@ -0,0 +1,262 @@
+<?php
+/*
+** 文件及文件夹处理类
+*/
+
+namespace hulang\tool;
+
+class File
+{
+    /**
+     * 创建目录
+     * @param $dir     string   目录名
+     * @return boolean true 成功， false 失败
+     */
+    public static function mk_dir($dir)
+    {
+        $dir = rtrim($dir, '/') . '/';
+        if (!is_dir($dir)) {
+            if (mkdir($dir, 0700) == false) {
+                return false;
+            }
+            return true;
+        }
+        return true;
+    }
+    /**
+     * 读取文件内容
+     * @param $filename  string  文件名
+     * @return string    文件内容
+     */
+    public static function read_file($filename)
+    {
+        $content = '';
+        if (function_exists('file_get_contents')) {
+            @($content = file_get_contents($filename));
+        } else {
+            if (@($fp = fopen($filename, 'r'))) {
+                @($content = fread($fp, filesize($filename)));
+                @fclose($fp);
+            }
+        }
+        return $content;
+    }
+    /**
+     * 写文件
+     * @param $filename  string  文件名
+     * @param $writetext string  文件内容
+     * @param $openmod 	 string  打开方式
+     * @return boolean true 成功, false 失败
+     */
+    public static function write_file($filename, $writetext, $openmod = 'w')
+    {
+        if (@($fp = fopen($filename, $openmod))) {
+            flock($fp, 2);
+            fwrite($fp, $writetext);
+            fclose($fp);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * 删除文件
+     * @param  $filename string  文件名
+     * @return boolean true 成功, false 失败
+     */
+    public static function del_file($filename)
+    {
+        if (file_exists($filename)) {
+            unlink($filename);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * 删除目录
+     * @param $dirName   string   原目录
+     * @return boolean true 成功, false 失败
+     */
+    public static function del_dir($dirName)
+    {
+        if (!file_exists($dirName)) {
+            return false;
+        }
+        $dir = opendir($dirName);
+        while ($fileName = readdir($dir)) {
+            $file = $dirName . '/' . $fileName;
+            if ($fileName != '.' && $fileName != '..') {
+                if (is_dir($file)) {
+                    self::del_dir($file);
+                } else {
+                    unlink($file);
+                }
+            }
+        }
+        closedir($dir);
+        return rmdir($dirName);
+    }
+    /**
+     * 复制目录
+     * @param $surDir   string   原目录
+     * @param $toDir  	string   目标目录
+     * @return boolean true 成功, false 失败
+     */
+    public static function copy_dir($surDir, $toDir)
+    {
+        $surDir = rtrim($surDir, '/') . '/';
+        $toDir = rtrim($toDir, '/') . '/';
+        if (!file_exists($surDir)) {
+            return false;
+        }
+        if (!file_exists($toDir)) {
+            self::mk_dir($toDir);
+        }
+        $file = opendir($surDir);
+        while ($fileName = readdir($file)) {
+            $file1 = $surDir . '/' . $fileName;
+            $file2 = $toDir . '/' . $fileName;
+            if ($fileName != '.' && $fileName != '..') {
+                if (is_dir($file1)) {
+                    self::copy_dir($file1, $file2);
+                } else {
+                    copy($file1, $file2);
+                }
+            }
+        }
+        closedir($file);
+        return true;
+    }
+    /**
+     * 得到指定目录里的信息
+     * @param $path   string   原目录
+     * @return unknown
+     */
+    public static function getFolder($path)
+    {
+        if (!is_dir($path)) {
+            return null;
+        }
+        $path = rtrim($path, '/') . '/';
+        $path = realpath($path);
+        $flag = \FilesystemIterator::KEY_AS_FILENAME;
+        $glob = new \FilesystemIterator($path, $flag);
+        $list = array();
+        foreach ($glob as $name => $file) {
+            $dir_arr = [];
+            $dir_arr['name'] = self::convertEncoding($file->getFilename());
+            if ($file->isDir()) {
+                $dir_arr['type'] = 'dir';
+                $dir_arr['size'] = self::fileSizeFormat(self::getDirSize($file->getPathname()));
+                $dir_arr['ext'] = '';
+            } else {
+                $dir_arr['type'] = 'file';
+                $dir_arr['size'] = self::fileSizeFormat($file->getSize());
+                $dir_arr['ext'] = $file->getExtension();
+            }
+            $dir_arr['path'] = $file->getPathname();
+            $dir_arr['atime'] = $file->getATime();
+            $dir_arr['mtime'] = $file->getMTime();
+            $dir_arr['ctime'] = $file->getCTime();
+            $dir_arr['is_readable'] = $file->isReadable();
+            $dir_arr['is_writeable'] = $file->isWritable();
+            $list[] = $dir_arr;
+        }
+        $list == 1 ? sort($list) : rsort($list);
+        return $list;
+    }
+    /**
+     * 统计文件夹大小
+     * @param $dir  string   目录名
+     * @return number 文件夹大小(单位 B)
+     */
+    public static function getDirSize($dir)
+    {
+        $dirlist = opendir($dir);
+        $dirsize = 0;
+        while (false !== ($folderorfile = readdir($dirlist))) {
+            if ($folderorfile != '.' && $folderorfile != '..') {
+                if (is_dir("{$dir}/{$folderorfile}")) {
+                    $dirsize += self::getDirSize("{$dir}/{$folderorfile}");
+                } else {
+                    $dirsize += filesize("{$dir}/{$folderorfile}");
+                }
+            }
+        }
+        closedir($dirlist);
+        return $dirsize;
+    }
+    /**
+     * 检测是否为空文件夹
+     * @param $dir  string   目录名
+     * @return boolean true 空， fasle 不为空
+     */
+    public static function empty_dir($dir)
+    {
+        return ($files = @scandir($dir)) && count($files) <= 2;
+    }
+    /**
+     * 文件大小格式
+     * @param $byte  int   大小
+     * @return string
+     */
+    public static function fileSizeFormat($byte)
+    {
+        if ($byte < 1024) {
+            $unit = 'B';
+        } else if ($byte < 10240) {
+            $byte = self::round_dp($byte / 1024, 2);
+            $unit = 'KB';
+        } else if ($byte < 102400) {
+            $byte = self::round_dp($byte / 1024, 2);
+            $unit = 'KB';
+        } else if ($byte < 1048576) {
+            $byte = self::round_dp($byte / 1024, 2);
+            $unit = 'KB';
+        } else if ($byte < 10485760) {
+            $byte = self::round_dp($byte / 1048576, 2);
+            $unit = 'MB';
+        } else if ($byte < 104857600) {
+            $byte = self::round_dp($byte / 1048576, 2);
+            $unit = 'MB';
+        } else if ($byte < 1073741824) {
+            $byte = self::round_dp($byte / 1048576, 2);
+            $unit = 'MB';
+        } else {
+            $byte = self::round_dp($byte / 1073741824, 2);
+            $unit = 'GB';
+        }
+        $byte .= $unit;
+        return $byte;
+    }
+    /**
+     * 辅助函数 round_up()，该函数用来取舍小数点位数的，四舍五入
+     */
+    public static function round_dp($num, $dp)
+    {
+        $sh = pow(10, $dp);
+        return (round($num * $sh) / $sh);
+    }
+    /**
+     * 获取文件扩展名
+     * @param $fileName  string   文件名
+     * @return string 扩展名
+     */
+    public static function getFileExt($fileName)
+    {
+        return strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    }
+    /**
+     * 转换字符编码
+     * @param $string
+     * @return string
+     */
+    public static function convertEncoding($string)
+    {
+        //根据系统进行配置
+        $encode = stristr(PHP_OS, 'WIN') ? 'GBK' : 'UTF-8';
+        $string = iconv($encode, 'UTF-8', $string);
+        return $string;
+    }
+}
---
+++ src/Tree.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace hulang\tool;
+
+/**
+ * 无限分级类
+ *
+ */
+class Tree
+{
+    /** 子孙树
+     * @param $data array  数据
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @param $pid     int    父级元素的id 实际上传递元素的主键
+     * @param $lv      int    级别
+     * @return array
+     */
+    public static function getSubTree($data, $parent = 'pid', $son = 'id', $pid = 0, $lv = 0)
+    {
+        $tmp = [];
+        foreach ($data as $k => $v) {
+            if ($v[$parent] == $pid) {
+                $v['lv'] = $lv;
+                $tmp[] = $v;
+                $tmp = array_merge($tmp, self::getSubTree($data, $parent, $son, $v[$son], $lv + 1));
+            }
+        }
+        return $tmp;
+    }
+    /**
+     * @param $data array  数据
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @param $pid     int    父级元素的id 实际上传递元素的主键
+     * @param $child   string 子标签包含名称默认：child
+     * @return array
+     */
+    public static function getSubTreeList($data, $parent = 'pid', $son = 'id', $pid = 0, $child = 'child')
+    {
+        $tmp = [];
+        foreach ($data as $k => $v) {
+            if ($v[$parent] == $pid) {
+                $v[$child] = self::getSubTreeList($data, $parent, $son, $v[$son], $child);
+                $tmp[] = $v;
+            }
+        }
+        return $tmp;
+    }
+    /** 组合一维数组
+     * @param $data    array  数据
+     * @param $html    string 层级标签
+     * @param $pid     int    顶级分类的值
+     * @param $level   int    等级
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @return array
+     */
+    public static function getOneMergeTree($data, $html = '├─', $pid = 0, $level = 0, $parent = 'pid', $son = 'id')
+    {
+        $arr = [];
+        foreach ($data as $k => $v) {
+            if ($v[$parent] == $pid) {
+                $v['level'] = $level + 1;
+                $v['html'] = str_repeat($html, $level);
+                $arr[] = $v;
+                $arr = array_merge($arr, self::getOneMergeTree($data, $html, $v[$son], $level + 1, $parent, $son));
+            }
+        }
+        return $arr;
+    }
+    /** 组合多维数组
+     * @param $data    array  数据
+     * @param $pid     int    顶级分类的值
+     * @param $name    string 二级数组的名称
+     * @return array
+     */
+    public static function getMultidMergeTree($data, $pid = 0, $name = 'child')
+    {
+        $arr = [];
+        foreach ($data as $k => $v) {
+            if ($v['pid'] == $pid) {
+                $v[$name] = self::getMultidMergeTree($data, $v['id'], $name);
+                $arr[] = $v;
+            }
+        }
+        return $arr;
+    }
+    /** 合并成父子树
+     * @param $data          array  数据
+     * @param $parent        string 父级元素的名称 如 pid
+     * @param $son           string 子级元素的名称 如 id
+     * @param $sort_type     int    二级数组排序方式
+     * @param $name          string 二级数组的名称
+     * @return array
+     */
+    public static function getTree($data, $parent = 'pid', $son = 'id', $sort_type = 0, $name = 'child')
+    {
+        $tmp = [];
+        if (!empty($data)) {
+            $fu = [];
+            $zi = [];
+            foreach ($data as $k => $v) {
+                if ($v[$parent] == 0) {
+                    $fu[] = $v;
+                } else {
+                    $zi[] = $v;
+                }
+            }
+            $arr = array_column($zi, $son);
+            if ($sort_type == 1) {
+                array_multisort($arr, SORT_ASC, $zi);
+            } else {
+                array_multisort($arr, SORT_DESC, $zi);
+            }
+            foreach ($zi as $k => $v) {
+                $key = array_search($v[$parent], $arr);
+                if ($key !== false) {
+                    $zi[$k][$parent] = $zi[$key][$parent];
+                }
+            }
+            $array = array_column($fu, $son);
+            foreach ($zi as $k => $v) {
+                $key = array_search($v[$parent], $array);
+                if ($key !== false) {
+                    $fu[$key][$name][] = $v;
+                }
+            }
+            $tmp = $fu;
+        }
+        return $tmp;
+    }
+    /** 传递子分类的id返回所有的父级分类
+     * @param $data    array  数据
+     * @param $id      int    子级元素id值
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @return array
+     */
+    public static function getParents($data, $id, $parent = 'pid', $son = 'id')
+    {
+        $arr = [];
+        foreach ($data as $k => $v) {
+            if ($v[$son] == $id) {
+                $arr[] = $v;
+                $arr = array_merge(self::getParents($data, $v[$parent], $parent, $son), $arr);
+            }
+        }
+        return $arr;
+    }
+    /** 传递子分类的id返回所有的父级分类
+     * @param $data    array  数据
+     * @param $id      int    子级元素id值
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @return array
+     */
+    public static function getParentsIds($data, $id, $parent = 'pid', $son = 'id')
+    {
+        $arr = [];
+        foreach ($data as $k => $v) {
+            if ($v[$son] == $id && $v[$parent] != 0) {
+                $arr[] = $v[$parent];
+                $arr = array_merge(self::getParentsIds($data, $v[$parent], $parent, $son), $arr);
+            }
+        }
+        return $arr;
+    }
+    /** 传递父级id返回所有子级id
+     * @param $data    array  数据
+     * @param $pid     int    父级元素id值
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @return array
+     */
+    public static function getChildsId($data, $pid, $parent = 'pid', $son = 'id')
+    {
+        $arr = [];
+        foreach ($data as $k => $v) {
+            if ($v[$parent] == $pid) {
+                $arr[] = $v[$son];
+                $arr = array_merge($arr, self::getChildsId($data, $v[$son], $parent, $son));
+            }
+        }
+        return $arr;
+    }
+    /** 传递父级id返回所有子级分类
+     * @param $data    array  数据
+     * @param $pid     int    父级元素id值
+     * @param $parent  string 父级元素的名称 如 pid
+     * @param $son     string 子级元素的名称 如 id
+     * @return array
+     */
+    public static function getChilds($data, $pid, $parent = 'pid', $son = 'id')
+    {
+        $arr = [];
+        foreach ($data as $k => $v) {
+            if ($v[$parent] == $pid) {
+                $arr[] = $v;
+                $arr = array_merge($arr, self::getChilds($data, $v[$son], $parent, $son));
+            }
+        }
+        return $arr;
+    }
+}
