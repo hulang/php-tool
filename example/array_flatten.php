99fb886481ed2de85b001651c189ae6c1da8b6e4
快乐浪子哥 <hulangfy@163.com>
2024-05-11 11:47:45 +0800

    update

--- example/array_flatten.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use hulang\tool\Arr;
-
-$arr = [1, 54, 'a', 45, 12, 'c', 1, 1, 12, [1, 1, 'a', ['a', 'b', 'a']]];
-Arr::dd(Arr::arrayFlatten($arr));
--- example/array_insert.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use hulang\tool\Arr;
-
-$arr = [1, 54, 'a', 45, 12, 'c', 1, 1, 12, [1, 1, 'a', ['a', 'b', 'a']]];
-
-Arr::dd(Arr::arrayInsert($arr, 23, 0));
