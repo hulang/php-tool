91c533dbe4f13d21509718d3f7ab63f3fc0d4efb
快乐浪子哥 <4961831+hulang@users.noreply.github.com>
2024-09-05 10:49:23 +0800

    Update

--- src/FileHelper.php
+++ src/FileHelper.php
@@ -348,8 +348,8 @@ class FileHelper
     public static function emptyDir($dir)
     {
         // 使用scandir函数扫描目录,并检查结果是否仅包含默认的两个项
-        $result = ($files = @scandir($dir)) && count($files) <= 2;
-        return $result;
+        $files = scandir($dir);
+        return count($files) == 2;
     }
 
     /**
